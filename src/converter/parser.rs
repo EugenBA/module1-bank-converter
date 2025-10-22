7aae4afebf140b592d2de1556ad2c98d47d507a8
eugen <b-eugen@yandex.ru>
2025-10-22 22:36:21 +0300

    add data struct

--- src/converter/parser.rs
+++ src/converter/parser.rs
@@ -7,6 +7,31 @@ pub enum FormatType {
 }
 
 struct BaseAttribute{
-    account: 
+    account_number: String,
+    account_owner: String,
+    currency: String,
+    address_owner: String,
+    bik_swift: String,
+    period_list: String,
     
+}
+
+struct BalanceInfo {
+}
+
+struct TransactionInfo {
+}
+
+struct ContragentInfo{
+    name: String,
+    inn: String,
+    kpp: String,
+    ogrn: String,
+    address: String,
+    bank: String,
+    country: String,
+}
+
+struct AdditionInfo{
+
 }
