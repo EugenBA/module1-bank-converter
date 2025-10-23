97fa911ee842c8aac0876892cda40ba69badced4
beugen <b-eugen@yandex.ru>
2025-10-23 15:50:41 +0300

    Add struct for CAMT053 format

--- src/converter/parser.rs
+++ src/converter/parser.rs
@@ -5,17 +5,80 @@ pub enum FormatType {
     Mt940,
     Camt053,
 }
-
-struct BaseAttribute{
-    account_number: String,
-    account_owner: String,
-    currency: String,
-    address_owner: String,
-    bik_swift: String,
-    period_list: String,
-    
+struct BkToCstmAttribute{
+    grp_hdr: HeaderAttribute,
+    stmt: StatementAttribute,
 }
 
+struct HeaderAttribute{
+    msg_id: String, //message id
+    crd_dt_tm: String, //datetime create file
+ }
+
+struct StatementAttribute{
+    id: String, //id
+    electr_seq_nb: u32, //ElctrncSeqNb
+    lgl_seq_nv: u32, //LglSeqNb
+    cred_dt_tm: String, //CreDtTm
+    fr_to_dt: FromToDtAttribute, //FrToDt
+    acct: AccAttribute, //Acct
+
+}
+
+struct FromToDtAttribute{
+    fr_dt_tm: String, //FrDtTm
+    to_dt_tm: String, //ToDtTm
+}
+
+struct AccAttribute{
+    id_iban: String, //id/IBAN
+    ccy: String, //Ccy
+    nm: String, //nm
+    ownr: OwnerAttribute, //ownr
+    svcr: SvcrAttribute, //svcr
+
+}
+
+struct SvcrAttribute{
+    fin_inst_id: FinInstIdAttribute//FinInstnId
+}
+
+struct FinInstIdAttribute{
+    bic: String, //BIC
+}
+
+struct OwnerAttribute{
+    nm: String, //nm
+    pstl_addr: PostalAddressAttribute, //pstl_addr
+    bldg_nb: u32, //BldgNb
+    pst_cd: u32, //PstCd
+    twn_nm: String, //TwnNm
+    ctry: String, //Ctry
+    id: IdAttribute, //Id
+}
+
+
+struct IdAttribute{
+    org_id: OrgIdAttribute//OrgId
+}
+
+struct OrgIdAttribute{
+    othr: OtherAttribute //Othr
+}
+
+struct OtherAttribute{
+    id: String, //id
+    schme_nm: ShemeNumberAttribute //SchmeNm
+}
+
+struct ShemeNumberAttribute{
+    cd: String, //cd
+}
+struct PostalAddressAttribute{
+    strt_nm: String, //strt_nm
+
+
+}
 struct BalanceInfo {
 }
 
