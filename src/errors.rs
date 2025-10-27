b0ce3aa8b5fcf8a0e619e497318e2ccd1ee303d3
eugen <b-eugen@yandex.ru>
2025-10-27 23:36:51 +0300

    add struct Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,12 +1,39 @@
+use std::fmt::{Display, Formatter};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
 enum CommandArgsError
 {
-    BadArgument,
-    BadFormatType
+    BadArgument(String),
+    BadFormatType(String)
 }
 
+
+#[derive(Error, Debug)]
 enum ParserError
 {
-    FileReadError,
-    BadInputFormatFile,
-    FileWriteError
+    FileReadError(String),
+    BadInputFormatFile(String),
+    FileWriteError(String)
 }
+
+impl Display for ParserError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParserError::FileReadError(s) => write!(f, "File read error: {}", s),
+            ParserError::BadInputFormatFile(s) => write!(f, "Bad input format file: {}", s),
+            ParserError::FileWriteError(s) => write!(f, "File write error: {}", s),
+        }
+    }
+}
+
+impl Display for CommandArgsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CommandArgsError::BadArgument(s) => write!(f, "Bad argument: {}", s),
+            CommandArgsError::BadFormatType(s) => write!(f, "Bad format type: {}", s),
+        }
+    }
+}
+
+
