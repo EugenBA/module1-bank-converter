d60c3a4fcb6d003eec9832a86ec8c98d12d094ca
beugen <b-eugen@yandex.ru>
2025-10-28 17:28:57 +0300

    Add error

--- src/converter/parser.rs
+++ src/converter/parser.rs
@@ -1,8 +1,7 @@
-use std::fs::File;
-use std::io;
+
 use std::io::{BufRead, BufReader, Stdin};
-use crate::errors::{ParserError};
 use crate::models::camt053::*;
+use crate::errors::{ParserError};
 
 pub enum FormatType {
     None,
@@ -24,45 +23,30 @@ impl Document {
     pub(crate) fn default() -> Self {
         todo!()
     }
-    
-    fn from_csv(buf_reader: Stdin) -> Result<Self, ParserError>{
-        Err(ParserError::BadInputFormatFile("Bad input format file".to_string()))
+
+    fn from_csv(buf_reader: Stdin) -> Result<Self>{
+
     }
-    fn from_camt053(buff_read: Stdin) -> Result<Self, ParserError> {
-        let mut reader = buff_read.lock();
-        let mut xml_str = String::new();
-        while let Ok(byte_reader) = reader.read_line(&mut xml_str) {
-            if byte_reader == 0 {
-                break;
-            }
-        }
-        match serde_xml_rs::from_str(&xml_str) {
-            Ok(doc) => {
-                Ok(doc)
-            },
-            Err(e) => {
-                Err(ParserError::BadInputFormatFile(e.to_string()))
-            }
-        }
+    fn from_camt053(buff_read: Stdin) -> Result<Self> {
+
+
     }
-    
-    fn from_mt940(buf_reader: Stdin) -> Result<Self, ParserError> {
-        Err(ParserError::BadInputFormatFile("Bad input format file".to_string()))
-    }
-    
-    
+
+    fn from_mt940(buf_reader: Stdin) -> Result<Self> {}
+
+
 }
 
 
-impl TryFrom<InputDataType> for Document {
-    type Error = ParserError;
-    fn try_from(value: InputDataType) -> Result<Self, ParserError> {
+
+impl From<InputDataType> for Document {
+    fn from(value: InputDataType) -> Result<Self> {
         match value.format_type {
             FormatType::Camt053 | FormatType::Xml =>{ Document::from_camt053(value.buff_read)},
             FormatType::Mt940 => { Document::from_mt940(value.buff_read)},
             FormatType::Csv => { Document::from_csv(value.buff_read)
             },
-            _ => { Err(ParserError::BadInputFormatFile("Bad input format file".to_string()))}
+            _ => { Err(ParserError::BadInputFormatFile(String::from("Unsupported format")))}
         }
     }
 }
